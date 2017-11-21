025b416d7f591a5b950f4bcaa72a54a96ced9c2e
Austin Thomas Griffith <me@austingriffith.com>
2017-11-21 04:10:11 -0700

    init

--- commands/init.js
+++ commands/init.js
@@ -1,11 +1,12 @@
 module.exports = (params)=>{
+  let ignore = params.fs.readFileSync(__dirname+"/../templates/gitignore").toString()
+  if(!params.fs.existsSync(".gitignore")) {
+    console.log("Adding .gitignore")
+    params.fs.writeFileSync(".gitignore",ignore);
+  }
   console.log("Creating config file: clevis.json")
   let init = params.fs.readFileSync(__dirname+"/../templates/config.json").toString()
   params.fs.writeFileSync("clevis.json",init);
-  let ignore = params.fs.readFileSync(__dirname+"/../templates/gitignore").toString()
-  if (params.fs.existsSync(".gitignore")) {
-    console.log("Adding .gitignore too...")
-    params.fs.writeFileSync(".gitignore",ignore);
-  }
+
   return init
 }
