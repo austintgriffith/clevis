c4972a2a8caa570c610028334de0f2592503670e
Austin Thomas Griffith <me@austingriffith.com>
2017-11-21 04:03:33 -0700

    extra gitignore in init

--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,11 @@
 module.exports = (params)=>{
+  console.log("Creating config file: clevis.json")
   let init = params.fs.readFileSync(__dirname+"/../templates/config.json").toString()
   params.fs.writeFileSync("clevis.json",init);
+  let ignore = params.fs.readFileSync(__dirname+"/../templates/gitignore").toString()
+  if (params.fs.existsSync(".gitignore")) {
+    console.log("Adding .gitignore too...")
+    params.fs.writeFileSync(".gitignore",ignore);
+  }
   return init
 }
