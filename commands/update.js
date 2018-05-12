85c42f52dc9e8eae46b68e8c954f087d5fa263bd
Ubuntu <ubuntu@ip-172-31-9-13.us-east-2.compute.internal>
2018-05-12 14:27:43 +0000

    do update in correct way

--- commands/update.js
+++ commands/update.js
@@ -1,20 +1,26 @@
 let axios = require('axios')
 let fs = require('fs')
+//usage: clevis update
 module.exports = async (params)=>{
-  //const DEBUG = params.config.DEBUG;
-  //if(DEBUG) console.log(" >>> ascii")
-  axios.get("https://ethgasstation.info/json/ethgasAPI.json")
-	.then((response)=>{
-    let newgas = response.data.average/10+0.1;
-    let clevis = JSON.parse(fs.readFileSync("clevis.json").toString())
-    clevis.gasprice = newgas
-    axios.get("https://api.coinmarketcap.com/v2/ticker/1027/")
-    .then((response)=>{
-      clevis.ethprice = response.data.data.quotes.USD.price
-      let result = JSON.stringify(clevis,null, 2)
-      console.log(result)
-      fs.writeFileSync("clevis.json",result)
-    })
-	})
-  return "Updating clevis.json...";
+  const DEBUG = params.config.DEBUG;
+  if(DEBUG) console.log(" >>> UPDATE")
+  return await update(params)
+}
+function update(params) {
+  const DEBUG = params.config.DEBUG;
+  return new Promise((resolve, reject) => {
+     axios.get("https://ethgasstation.info/json/ethgasAPI.json")
+   	.then((response)=>{
+       let newgas = response.data.average/10+0.1;
+       let clevis = JSON.parse(fs.readFileSync("clevis.json").toString())
+       clevis.gasprice = newgas
+       axios.get("https://api.coinmarketcap.com/v2/ticker/1027/")
+       .then((response)=>{
+         clevis.ethprice = response.data.data.quotes.USD.price
+         let result = JSON.stringify(clevis,null, 2)
+         fs.writeFileSync("clevis.json",result)
+         resolve(result)
+       })
+   	})
+  })
 }
