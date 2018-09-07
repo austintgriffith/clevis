1f542e38dd89fff96e1298e97a00f3c4e7d03884
David Rhodus <david.rhodus859@gmail.com>
2018-09-07 14:25:31 +0200

    Lock in the latest release version of Open Zeppelin

--- commands/init.js
+++ commands/init.js
@@ -39,7 +39,7 @@ module.exports = async (params)=>{
   //installing node module locally//
   console.log("Installing clevis (this will take a while to compile)...")
 
-  exec('chmod +x *.sh;npm install --save clevis@latest;npm install --save s3;npm install --g mocha;git clone https://github.com/OpenZeppelin/openzeppelin-solidity.git;cd openzeppelin-solidity git pull', (err, stdout, stderr) => {
+  exec('chmod +x *.sh;npm install --save clevis@latest;npm install --save s3;npm install --g mocha;git clone https://github.com/OpenZeppelin/openzeppelin-solidity.git;cd openzeppelin-solidity git pull; git checkout tags/v1.12.0', (err, stdout, stderr) => {
     exec('clevis update', (err, stdout, stderr) => {}).stdout.on('data', function(data) {
         console.log(data)
     })
