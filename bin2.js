cbe609c4ad731eb5c879931678700fcf617f4453
dbe <davidbrianethier@gmail.com>
2019-01-10 12:33:11 -0700

    Taking logging init to other file

--- bin2.js
+++ bin2.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const program = require('commander');
 const winston = require('winston');
-configureLogger();
+require('./initLogger')()
 
 program
   .option('--debug', 'Turns on Debugging output')
@@ -11,15 +11,15 @@ program
 
 program.command('fromWei2 <amount> <symbol>').action(standard);
 
-// program
-//   .command('airdrop')
-//   .description('Drop ETH or ERC20 tokens onto many accounts.')
-//   .option('-f, --from-index <fromIndex>', "The local clevis account index to send funds from.")
-//   .option('-d, --dryrun', 'Prints out what will be done without actually sending transactions.')
-//   .action(standard);
+program.on('command:*', () => {
+  console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
+  process.exit(1);
+});
 
 program.parse(process.argv)
-
+if(program.args.length == 0) {
+  program.help();
+}
 
 //Default handler when no extra logic is required
 function standard(...args) {
@@ -43,21 +43,5 @@ function runCmd(name, args) {
     web3: require('web3')
   }
 
-  let result = require(`./commands/${name}.js`)(...args, params);
-  console.log(result);
-}
-
-//TODO: Can take this out of this file as long as its called once
-function configureLogger() {
-  winston.addColors({debug: 'magenta'});
-  winston.configure({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    ),
-    transports: [
-      new winston.transports.Console()
-    ]
-  });
+  console.log(require(`./commands/${name}.js`)(...args, params))
 }
