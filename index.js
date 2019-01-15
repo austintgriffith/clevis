e671921ba84d009cb4caac3fdbcad65e92c08289
dbe <davidbrianethier@gmail.com>
2019-01-15 11:44:27 -0700

    Fixing index.js again

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const runClevis = require('./clevis')
 
-// function clevis(...args) {
-//   console.log('in index clevis args: ', args);
-//   let call = process.argv[0]
-//   let path = process.argv[1] || __dirname
-//
-//   return runClevis(call, path, ...args)
-// }
-
 let { exec } = require('child_process')
 async function clevis(...args) {
-  let command = './bin.js ' + args.join(' ')
+  let command = __dirname + '/bin.js ' + args.join(' ')
 
   return new Promise((resolve, reject) => {
     exec(command, function(e, out, err) {
