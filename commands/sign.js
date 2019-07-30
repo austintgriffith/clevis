c14b475f4c4351a1bd2beff3821414410e5a0cff
Austin <austin.griffith@madwiremedia.com>
2019-07-30 17:53:37 -0600

    detect password and sign in different ways

--- commands/sign.js
+++ commands/sign.js
@@ -4,7 +4,9 @@ module.exports = async (string, accountIndex, password, params)=>{
   if(address === undefined) {
     throw(`accountIndex: ${accountIndex} is undefined`)
   }
+  console.log("password is ",password)
   if(typeof password == "undefined"){
+    console.log("using personal sign")
     return params.web3.eth.personal.sign(string, address, password)
   }else{
     return params.web3.eth.sign(string, address)
